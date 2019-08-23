61d6f3f5d563fbe7bb40fcc0e3ab5450b2caa37f
Rita <paradise0917@gmail.com>
2019-08-23 12:52:57 +0800

    Feature: Add support for changing tile position

--- src/Game/index.js
+++ src/Game/index.js
@@ -12,19 +12,19 @@ const Game = (props) => {
     //     setUrgent(false);
     // };
 
-    const showtile = (val) => {
-        return <div key={val} className="tile flex flex-jus-center flex-ali-center">
-            <span>{val}</span>
+    const showtile = (item) => {
+        return <div key={item.value} onClick={() => {props.checkCanSlack(item);}} className="tile flex flex-jus-center flex-ali-center">
+            <span>{item.value}</span>
         </div>};
 
-    const hidetile = (val) => {
-        return <div key={val} className="tile flex flex-jus-center flex-ali-center">
+    const hidetile = (item) => {
+        return <div key={item.value} className="tile tile-empty flex flex-jus-center flex-ali-center">
             <span></span>
         </div>};
 
     const TotalTile = props.tileArray.length ? 
     (props.tileArray.map(item => { 
-        return (item.value < 9) ? showtile(item.value) : hidetile(item.value)
+        return (item.value < 9) ? showtile(item) : hidetile(item)
     })) : (<div>Empty Tile Array</div>);
     return <div className="outer flex flex-jus-center flex-ali-center"><div className="panel flex flex-jus-center flex-ali-center flex-flow-wrap">{TotalTile}</div></div>;
 }
--- src/index.js
+++ src/index.js
@@ -17,25 +17,93 @@ class App extends React.Component {
         this.state = { 
             size: 3,
             tileArray :[
-                {"row":0 , "column":0 , "value":1, "hidden":false},
-                {"row":0 , "column":1 , "value":2, "hidden":false},
-                {"row":0 , "column":2 , "value":3, "hidden":false},
-                {"row":1 , "column":0 , "value":4, "hidden":false},
-                {"row":1 , "column":1 , "value":5, "hidden":false},
-                {"row":1 , "column":2 , "value":6, "hidden":false},
-                {"row":2 , "column":0 , "value":7, "hidden":false},
-                {"row":2 , "column":1 , "value":8, "hidden":false},
-                {"row":2 , "column":2 , "value":9, "hidden":true}
+                {row:0 , col:0 , value:1},
+                {row:0 , col:1 , value:2},
+                {row:0 , col:2 , value:3},
+                {row:1 , col:0 , value:4},
+                {row:1 , col:1 , value:5},
+                {row:1 , col:2 , value:6},
+                {row:2 , col:0 , value:7},
+                {row:2 , col:1 , value:8},
+                {row:2 , col:2 , value:9}
             ]
         };
         // this.changeCategory = this.changeCategory.bind(this);
         // this.changeStatus = this.changeStatus.bind(this);
         // this.addTask = this.addTask.bind(this);
         // this.showAddTask = this.showAddTask.bind(this);
+        this.checkCanSlack = this.checkCanSlack.bind(this);
+        this.checkTop = this.checkTop.bind(this);
+        this.checkRight = this.checkRight.bind(this);
+        this.checkBottom = this.checkBottom.bind(this);
+        this.checkLeft = this.checkLeft.bind(this);
     }
 
-    showAddTask(state){
-        this.setState({ showAddPanel: state });
+    checkCanSlack(clickitem){
+        console.log(clickitem);
+        const checkTopResult = this.checkTop(clickitem.row, clickitem.col);
+        const checkRightResult = this.checkRight(clickitem.row, clickitem.col);
+        const checkBottomResult = this.checkBottom(clickitem.row, clickitem.col);
+        const checkLeftResult = this.checkLeft(clickitem.row, clickitem.col);
+        if(checkTopResult || checkRightResult || checkBottomResult || checkLeftResult){
+
+            const clickval = clickitem.value;
+            const emptyItem = this.state.tileArray.filter(item => item.value === 9)[0];
+            const emptyIndex = this.state.tileArray.indexOf(emptyItem);
+            this.state.tileArray[emptyIndex].value = clickval;
+
+            const itemIndex = this.state.tileArray.indexOf(clickitem);
+            this.state.tileArray[itemIndex].value = 9;
+
+            // Update State
+            console.log(this.state.tileArray);
+            this.setState({ tileArray: this.state.tileArray });
+        }
+
+    }
+
+    checkTop(curRow, curCol){
+        const newRow = curRow - 1;
+        if(newRow >=0){
+            const TopTile = this.state.tileArray.filter(item => item.row === newRow && item.col === curCol)[0];
+            if(TopTile.value === 9){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    checkRight(curRow, curCol){
+        const newCol = curCol + 1;
+        if(newCol < this.state.size){
+            const RightTile = this.state.tileArray.filter(item => item.row === curRow && item.col === newCol)[0];
+            if(RightTile.value === 9){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    checkBottom(curRow, curCol){
+        const newRow = curRow + 1;
+        if(newRow < this.state.size){
+            const BottomTile = this.state.tileArray.filter(item => item.row === newRow && item.col === curCol)[0];
+            if(BottomTile.value === 9){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    checkLeft(curRow, curCol){
+        const newCol = curCol - 1;
+        if(newCol >= 0){
+            const BottomTile = this.state.tileArray.filter(item => item.row === curRow && item.col === newCol)[0];
+            if(BottomTile.value === 9){
+                return true;
+            }
+        }
+        return false;
     }
 
     render(){
@@ -43,7 +111,7 @@ class App extends React.Component {
             <React.Fragment>
                 <BrowserRouter>
                     <Navbar />
-                    <Route path="/" exact render={() => <Game size={this.state.size} tileArray={this.state.tileArray} />} />   
+                    <Route path="/" exact render={() => <Game size={this.state.size} tileArray={this.state.tileArray} checkCanSlack={this.checkCanSlack} />} />   
                     <Route path="/rank" render={() => <Rank />}/>   
                 </BrowserRouter>
             </React.Fragment>
